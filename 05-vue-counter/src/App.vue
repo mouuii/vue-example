480381df4b731419b719094e50b60009cee139b0
mouuii <49775493+mouuii@users.noreply.github.com>
2023-08-23 18:12:54 +0800

    05-vue-js

---
+++ 05-vue-counter/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import {ref} from "vue"
+  const count = ref(0);
+</script>
+
+<template>
+  <main>
+  <div>
+    <h4>
+      the corrent count is ...
+    </h4>
+    <h1>{{ count }}</h1>
+    <button @click="count--">-</button>
+    <button @click="count++">+</button>
+  </div>
+</main> 
+</template>
+
+<style scoped>
+main {
+  background-color: aquamarine;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+div{
+  text-align: center;
+}
+</style>
